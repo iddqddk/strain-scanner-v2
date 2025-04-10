44e434e5ec05ec2b23a721a135668be44312ae88
ODP <oscar@dollas.dk>
2025-04-10 13:37:49 +0200

    Add PWA support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <meta name="theme-color" content="#10B981" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
